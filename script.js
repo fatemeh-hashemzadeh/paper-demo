cf6fb871121edc9dc0ca8d85f0e4ca5df78bbbfa
fatemeh hashemzadeh <fatemeh.hashemzadeh2022@gmail.com>
2025-12-18 18:17:15 +0330

    paper-demo

---
+++ script.js
@@ -0,0 +1,68 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
+
+
+
+function playGame(playerChoice) {
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if (playerChoice === computerChoice) {
+        result = "IT`S A TIE!";
+    } else {
+        switch (playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!"; break;
+
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!"; break;
+
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!"; break;
+
+        }
+    }
+
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("greenText", "redText");
+
+    switch (result) {
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+
+
+    }
+}
+function resetGame() {
+    // بازنشانی امتیازها
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+
+    // پاکسازی نمایش انتخابها و نتیجه
+    playerDisplay.textContent = "";
+    computerDisplay.textContent = "";
+    resultDisplay.textContent = "";
+    resultDisplay.classList.remove("greenText", "redText");
+}
